c4739ada27cf4ddd190a94cf1802831b5aa34100
McPlayHD <mcplayhd@mcplayhd.net>
2016-07-16 12:44:36 +0200

    Erster Versuch, einen Zauberstab zu machen

--- src/me/mcplayhd/zauberei/Listeners/PlayerListener.java
+++ src/me/mcplayhd/zauberei/Listeners/PlayerListener.java
@@ -1,5 +1,46 @@
 package me.mcplayhd.zauberei.Listeners;
 
-public class PlayerListener {
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
+import me.mcplayhd.zauberei.Zauberei;
+
+public class PlayerListener implements Listener {
+
+	Zauberei plugin;
+	
+	public PlayerListener(Zauberei plugin) {
+		this.plugin = plugin;
+	}
+	
+	@EventHandler
+	public void onInteract(PlayerInteractEvent e) {
+		Player p = e.getPlayer();
+		if(e.getAction() == Action.RIGHT_CLICK_AIR) {
+			if(e.getItem().getType() == Material.STICK) {
+				final Location startloc = p.getEyeLocation();
+				for(int i = 1; i < 15; i += 0.5) {
+					Location loc = startloc.clone().add(startloc.clone().multiply(i));
+					loc.getWorld().spawnParticle(Particle.SPELL_WITCH, loc, 10, 0.12, 0.12, 0.12);
+					if(loc.getBlock().getType() != Material.AIR) {
+						for(Player alle : loc.getWorld().getPlayers()) {
+							if(alle.getLocation().distance(loc) < 3 || p.getEyeLocation().distance(loc) < 3) {
+								alle.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 20*5, 0));
+							}
+						}
+						break;
+					}
+				}
+			}
+		}
+	}
+	
 }
--- src/me/mcplayhd/zauberei/Zauberei.java
+++ src/me/mcplayhd/zauberei/Zauberei.java
@@ -2,8 +2,12 @@ package me.mcplayhd.zauberei;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.mcplayhd.zauberei.Listeners.PlayerListener;
+
 public class Zauberei extends JavaPlugin {
 
-	
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
+	}
 	
 }
